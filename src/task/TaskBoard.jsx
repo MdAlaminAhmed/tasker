c95eb76b9ebe90a87f3f7587772a2d59acf5d590
MdAlaminAhmed <contact.alamin02@gmail.com>
2025-11-13 18:53:21 +0600

    first commit

---
+++ src/task/TaskBoard.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+import AddTaskModal from "./AddTaskModal";
+import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
+import TaskList from "./TaskList";
+
+export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React Native",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavourite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask(newTask) {
+    console.log("adding tasks...", newTask);
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false)
+
+  }
+
+  return (
+    <>
+      <section className="mb-20" id="tasks">
+        {showAddModal
+        
+        && <AddTaskModal onSave={handleAddTask} />}
+        <div className="container">
+          <div className="p-2 flex justify-end">
+            <SearchTask />
+          </div>
+
+          <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+            <TaskActions onAddClick={() => setShowAddModal(true)} />
+            <TaskList tasks={tasks} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
